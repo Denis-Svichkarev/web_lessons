997e97829d3b5274072c30ef7727775fe6bd4bd4
Denis Svichkarev <denis.svichkarev@gmail.com>
2017-04-21 21:23:13 +0300

    - created vhost;

--- scripts/login.js
+++ scripts/login.js
@@ -24,3 +24,19 @@ function auth(form) {
         return false;
     }
 }
+
+
+function ajaxGet() {
+
+	var request = new XMLHttpRequest();
+	
+	request.onreadystatechange = function() {  
+
+		if (request.readyState == 4 && request.status == 200) { 
+            alert("Ответ сервера: " + request.response);
+		}
+	}
+
+	request.open('GET', 'http://scripts/vote.php');  
+	request.send(); 
+}
